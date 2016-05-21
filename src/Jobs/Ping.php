174efb2221248c1d4248816a44e7ed83cba0e1a6
crynobone <crynobone@gmail.com>
2016-05-21 18:34:03 +0800

    Add support for job.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Jobs/Ping.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Orchestra\Webhook\Jobs;
+
+use RuntimeException;
+use Illuminate\Support\Str;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class Ping implements ShouldQueue
+{
+    use Queueable, InteractsWithQueue, SerializesModels;
+
+    /**
+     * HTTP Method.
+     *
+     * @var string
+     */
+    protected $method;
+
+    /**
+     * Request URI.
+     *
+     * @var string
+     */
+    protected $uri;
+
+    /**
+     * Request Data.
+     *
+     * @var mixed
+     */
+    protected $data = [];
+
+    /**
+     * Request headers.
+     *
+     * @var array
+     */
+    protected $headers = [];
+
+    /**
+     * Contruct a new Ping.
+     *
+     * @param string  $method
+     * @param string  $uri
+     * @param mixed  $data
+     * @param array  $headers
+     */
+    public function __contruct($method, $uri, $data = [], array $headers = [])
+    {
+        $this->method  = Str::upper($method);
+        $this->uri     = $uri;
+        $this->data    = $data;
+        $this->headers = $headers;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $client = app('orchestra.webhook');
+
+        $response = $client->send($this->method, $this->uri, $this->data, $this->headers);
+
+        if ($response->getStatusCode() !== 200) {
+            throw new RuntimeException($response->getReasonPhrase(), $response->getStatusCode());
+        }
+    }
+}
